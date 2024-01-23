7fbebe110eb94692ddd190cc5e749dcaeac6a96f
Yauhen Yavorski <yauheny@uia.no>
2024-01-23 19:52:04 +0100

    ♻️ Refactor BathtubModel and add CournotCompetition class

--- Plant/__init__.py
+++ Plant/__init__.py
@@ -1,1 +1,2 @@
-from .BathtubModel import BathtubModel
+from .bathtub_model import BathtubModel
+from .cournot_competition import CournotCompetition
---
+++ Plant/cournot_competition.py
@@ -0,0 +1,26 @@
+class CournotCompetition:
+    def __init__(self):
+        pass
+
+    def update(self, signal: float, noise: float = 0.01) -> float:
+        # at each timestep:
+
+        # 1. q_1 updates based on U
+
+        # 2. q_2 updates based on D
+
+        # 3. q = q_1 + q_2
+
+        # 4. p(q) = p_max - q (assume p_max = 1)
+
+        # then on each timestep, producer 1's profit is: P_1 = p(q) * (q_1 - c_m)
+        # is the marginal cost: the cost to produce each item, independent of the total number produced.
+
+        # In this model, the target value (T) denotes the goal profit for each timestep. Thus:
+        # E = T - P_1
+
+        # and the error (E) serves as input to the controller on each timestep.
+        # In this model, cm can be a small fraction, such as 0.1, but you are free to experiment with different values
+        # of it along with values of pmax and T
+
+        pass
