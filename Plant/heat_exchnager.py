db3cdefa6018d4e71530e75ba7ee716e2c8a9afb
Yauhen Yavorski <yauheny@uia.no>
2024-01-28 14:47:15 +0100

    🐛 ♻️ Refactor HeatExchanger class to calculate delta temperature and heat transfer separately as well as fixing calculation of heat equation

--- Plant/heat_exchnager.py
+++ Plant/heat_exchnager.py
@@ -13,6 +13,8 @@ class HeatExchanger(Plant):
     heat_capacity = float(environ.get("HEAT_CAPACITY"))
     heat_transfer_area = float(environ.get("HEAT_TRANSFER_AREA"))
 
+    previous_temperature = initial_state
+
     def __init__(self, temperature: float = initial_state) -> None:
         """
         Initialize the HeatExchanger class.
@@ -41,26 +43,41 @@ class HeatExchanger(Plant):
         :param noise: random noise affecting the system
         :return: current temperature of the system
         """
-        # Calculate heat transfer
-        heat_transfer = (
-            self.heat_transfer_coefficient
-            * self.heat_transfer_area
-            * (self.target - self.temperature)
-        )
-
         # Calculate change in temperature
         # equation: Q = m * c * delta_T, solve for delta_T
         # https://www.geeksforgeeks.org/heat-transfer-formulas/
-        delta_temperature = (signal + noise - heat_transfer) / self.heat_capacity
+        delta_temperature = self.__calculate_delta_temperature(signal, noise)
 
         # Update temperature
         self.temperature += delta_temperature
 
+        self.previous_temperature = self.temperature
+
         return self.temperature
 
-    def get_temperature(self) -> float:
+    def __calculate_delta_temperature(self, signal: float, noise: float) -> float:
         """
-        Get the current temperature of the HeatExchanger.
-        :return: current temperature
+        Calculate the change in temperature.
+        equation: Q = m * c * delta_T, solve for delta_T
+        https://www.geeksforgeeks.org/heat-transfer-formulas/
+        :param signal: input signal affecting the system
+        :param noise: random noise affecting the system
+        :return: change in temperature
         """
-        return self.temperature
+        return (signal + noise - self.__calculate_heat_transfer()) / self.heat_capacity
+
+    def __calculate_heat_transfer(self) -> float:
+        """
+        Calculate the heat transfer.
+        equation: Q = h * A * delta_T
+        Q: heat transfer
+        h: heat transfer coefficient
+        A: heat transfer area
+        delta_T: change in temperature
+        :return: heat transfer
+        """
+        return (
+            self.heat_transfer_coefficient  # h
+            * self.heat_transfer_area  # A
+            * (self.previous_temperature - self.temperature)  # delta_T
+        )
