e877b542b05121dd9f4edfbce9035574f82bf03b
Yauhen Yavorski <yauheny@uia.no>
2024-01-29 23:02:05 +0100

    ♻️ Refactor generate_random_values function to use uniform function with minval and maxval parameters

--- utils/noise.py
+++ utils/noise.py
@@ -6,9 +6,6 @@ def generate_random_values(size, lower_bound, upper_bound):
     key = PRNGKey(0)
 
     # Generate random values between 0 and 1
-    random_values = uniform(key, shape=(size,), dtype=jnp.float32)
-
-    # Scale and shift the values to the desired range
-    scaled_values = lower_bound + random_values * (upper_bound - lower_bound)
-
-    return scaled_values
+    return uniform(
+        key, shape=(size,), dtype=jnp.float32, minval=lower_bound, maxval=upper_bound
+    )
